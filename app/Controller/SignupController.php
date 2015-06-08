a32cbb4f62cc3954baf8f65c31a86d0407439d94
taketnak <taketnak@gmail.com>
2015-06-08 14:59:28 +0900

    signupControllerのコードup

--- app/Controller/SignupController.php
+++ app/Controller/SignupController.php
@@ -36,9 +36,90 @@ class SignupController extends AppController{
 						'rule' => 'isUniqueAndActive',
 						'message' => 'すでに使用されています'
 						)
-					)
+			)
+		);
+
+
+		$this->User->set($this->request->data);
+		if(! $this->User->invalidField()){
+			$email = $this->request->data['User']['email'];
+			$activation_code = md5($email.time());
+			$user = $this->User->find('first',array(
+				'conditions' => array(
+								'email' => $email,
+								'is_active' => false)
+				)
 			);
-		
+
+			if(! $user){
+				$this->User->create();
+				$user = array('User' => $this->request->data['User']);
+			}
+			$user['User']['is_active'] = false;
+			$user['User']['activation_code'] = $activation_code;
+			$this->User->save($user);
+
+			$cakeemail = new CakeEmail('default');
+			$cakeemail->to($email);
+			$cakeemail->subject('仮登録のお知らせ');
+			$cakeemail->send(sprintf(
+				'http://example.com/signup/activate/%s',
+				$activation_code));
+			$this->render('email_sent');
+		}
 	}
+
+	public function activate($activation_code){
+		$user = $this->User->find('first',
+			array(
+				'conditions' => array(
+					'activation_code' => $activation_code,
+					'is_active' => false
+					)
+				)
+			);
+		if(!$user){
+			$this->redirect('/signup/index');
+		}
+
+		if(!$this->request->data){
+			$this->render();
+			return;
+		}
+		$this->User->validate = array(
+			'password' => array(
+				array(
+					'rule' => 'notEmpty',
+					'mesage' => 'パスワードを入力して下さい'
+				),
+				array(
+					'rule' => array('custom','/^[a-zA-Z0-9]+$/'),
+					'message' => '半角英数字で入力してください'
+				),
+				array(
+					'rule' => 'confirm',
+					'message' => 'パスワードが一致していません',
+				)
+			),
+		);
+		$this->User->set($this->request->data);
+		if(! $this->User->invalidFields()){
+			$passwordHasher = new SimplePasswordHasher();
+			$user['User']['password'] = $passwordHasher->hash(
+				$this->request->data['User']['password']
+			);
+
+			unset($user['User']['password_confirm']);
+			$user['User']['is_active'] = true;
+
+			$this->User->validate = array(); // パスワード暗号化でconfirmと一致しないためここでValidate解除
+			$this->User->save($user);
+
+			$this->Auth->login($user);
+			$this->redirect($this->Auth->redirectUrl());
+
+		}
+	}
+
 
 }
