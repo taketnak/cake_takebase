5de02ce7a1e660b8f44371f03904ea11bc560bd3
taketnak <taketnak@gmail.com>
2015-06-08 13:09:38 +0900

    Userモデルのコミット

--- app/Model/User.php
+++ app/Model/User.php
@@ -7,7 +7,31 @@ class User extends AppModel{
 
 public $name = 'User';
 
+	// 既に本登録済みのユーザがいないかチェックする
+	public function isUniqueAndActive($check){
+		foreach (check as $key => $value) {
+			$count = $this->find('count',array(
+				'conditions' => array(
+									$key => value,'is_active' => true,
+									),
+				'recursive' => -1
+				));
+			if($count != 0){ return false;}
+		}
+			return true;
+	}
 
 
+	public function conrirm($check){
+		foreach ($check as $key => $value) {
+			if(! isset($this->data[$this->name][$key.'_confirm'])){
+				return false;
+			}
+			if($value !== $this->data[$this->name][$key.'_confirm']){
+				return false;
+			}
+		}
+		return true;
+	}
 
 }
